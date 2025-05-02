ec4eecc34fdeef519e78941566152aeaadb75b4d
elvisrincon1 <elvisrincon1@gmail.com>
2025-05-01 20:59:57 -0400

    Add project/src/mock/products.js 1746147595992

---
+++ project/src/mock/products.js
@@ -0,0 +1,9 @@
+const products = [
+  { id: 1, name: "AF-Laptop", buyPrice: 500, sellPrice: 800, providers: [1, 2] },
+  { id: 2, name: "AF-Phone", buyPrice: 300, sellPrice: 500, providers: [1, 3] },
+  { id: 3, name: "Tablet", buyPrice: 200, sellPrice: 350, providers: [2] },
+  { id: 4, name: "AF-Headphones", buyPrice: 50, sellPrice: 120, providers: [3] },
+  { id: 5, name: "Monitor", buyPrice: 150, sellPrice: 250, providers: [1, 2, 3] }
+];
+
+export default products;
