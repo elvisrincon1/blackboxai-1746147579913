876bd18274b3ad14db05bf3b834625aea105933a
elvisrincon1 <elvisrincon1@gmail.com>
2025-05-01 20:59:58 -0400

    Add project/src/mock/sellers.js 1746147597703

---
+++ project/src/mock/sellers.js
@@ -0,0 +1,8 @@
+const sellers = [
+  { id: 1, name: "Godsplan" },
+  { id: 2, name: "Juan Perez" },
+  { id: 3, name: "Maria Garcia" },
+  { id: 4, name: "Carlos Lopez" }
+];
+
+export default sellers;
